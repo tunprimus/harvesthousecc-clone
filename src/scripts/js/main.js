a01e18c151cb6b19864d438580e565debabd8ce4
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-11-25 18:05:18 +0100

    feat: Add auto-update YouTube videos functionality

--- src/scripts/js/main.js
+++ src/scripts/js/main.js
@@ -1,72 +1,37 @@
 // @ts-check
 
-const REV_CHANNEL_URL = 'https://www.youtube.com/@NiyiEboda/videos';
-const SEEK_ID = 'video-title-link';
+const REV_CHANNEL_VIDEOS_URL = 'https://www.youtube.com/@NiyiEboda/videos';
+const SEEK_ID = '#video-title-link';
+const REV_CHANNEL_ID = 'UCWp3V0pGzOZy_an6ZsSUFiw';
+const REV_CHANNEL_RSS_URL = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCWp3V0pGzOZy_an6ZsSUFiw';
 
-/**
- * Checks if DOMParser is supported before using it to convert streams into HTML
- * @returns boolean
- */
-var supportStreamParse = (function () {
-  if (!window.DOMParser) {
-    return false;
-  }
-  var parser = new DOMParser();
-  try {
-    parser.parseFromString('buffer', 'text/html');
-  } catch(err) {
-    return false;
-  }
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+};
 
-  return true;
-})();
 
-/**
- * Converts stream or template string into HTML DOM nodes
- * @param {string} stream - The stream string
- * @returns {Node} The template HTML
- */
-function streamToHTML(stream) {
-  /**
-   * If DOMParser is supported, use it
-   */
-  if (supportStreamParse) {
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(stream, 'text/html');
-    return doc.body;
-  }
+const updateIframeVideos = (iframe) => {
+  const cid = REV_CHANNEL_ID;
+  const channelURL = encodeURIComponent(`https://www.youtube.com/feeds/videos.xml?channel_id=${cid}`);
+  const reqURL = `https://api.rss2json.com/v1/api.json?rss_url=${channelURL}`;
 
-  /**
-   * Otherwise, fallback to old-school method
-   */
-  var dom = document.createElement('div');
-  dom.innerHTML = stream;
-  return dom;
+  // @ts-ignore
+  fetch(reqURL, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+      const videoNumber = (iframe.getAttribute('video-number') ? Number(iframe.getAttribute('video-number')) : 0);
+      const link = result.items[videoNumber].link;
+      const videoTitle = result.items[videoNumber].title;
+      const id = link.substr(link.indexOf('=') + 1);
+      iframe.setAttribute('src', `https://youtube.com/embed/${id}?controls=0&autoplay=1`);
+      iframe.setAttribute('title', `${videoTitle}`);
+    })
+    .catch(err => console.error('Error:', err));
+};
+
+const iFrameElements = document.getElementsByClassName('sermon-iframe');
+for (let i = 0; i < iFrameElements.length; i++) {
+  updateIframeVideos(iFrameElements[i]);
 }
-
-/**
- * 
- * @param {string} url - The URL to fetch the video from
- * @param {string} keyId - The unique id of the div containers
- * @param {number} numberOfTitles - The number of titles to retrieve
- */
-function fetchYouTubeVideoTitles(url, keyId, numberOfTitles = 4) {
-  fetch(url)
-    .then(function(response) {
-      return response.text();
-    })
-    .then(function(html) {
-      var doc = streamToHTML(html);
-      // var titleContainers = doc.querySelectorAll(keyId);
-      var limitedTitleContainers = [];
-      for (var i = 0; i < numberOfTitles; i++) {
-        // limitedTitleContainers.push(titleContainers[i]);
-      }
-      console.log(limitedTitleContainers);
-    })
-    .catch(function(err) {
-      console.error('Failed to fetch page: ', err);
-    })
-}
-
-console.table(fetchYouTubeVideoTitles(REV_CHANNEL_URL, SEEK_ID, 4));
