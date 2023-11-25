3834cb5d83b88d64516ef149e38a6ec638540d46
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-11-25 18:14:01 +0100

    chore: Rename node element to prevent future name clash

--- src/scripts/js/main.js
+++ src/scripts/js/main.js
@@ -10,7 +10,13 @@ const requestOptions = {
   redirect: 'follow',
 };
 
-
+/**
+ * Function to update the videos displayed on the webpage
+ * Modified from
+ * @author Julia Furst Morgado
+ * @address https://dev.to/juliafmorgado/automatically-embed-latest-youtube-videos-on-your-website-with-javascript-step-by-step-2i5e
+ * @param {object} iframe - Node object to update the title and video source
+ */
 const updateIframeVideos = (iframe) => {
   const cid = REV_CHANNEL_ID;
   const channelURL = encodeURIComponent(`https://www.youtube.com/feeds/videos.xml?channel_id=${cid}`);
@@ -20,7 +26,7 @@ const updateIframeVideos = (iframe) => {
   fetch(reqURL, requestOptions)
     .then(response => response.json())
     .then(result => {
-      console.log(result);
+      
       const videoNumber = (iframe.getAttribute('video-number') ? Number(iframe.getAttribute('video-number')) : 0);
       const link = result.items[videoNumber].link;
       const videoTitle = result.items[videoNumber].title;
@@ -31,7 +37,7 @@ const updateIframeVideos = (iframe) => {
     .catch(err => console.error('Error:', err));
 };
 
-const iFrameElements = document.getElementsByClassName('sermon-iframe');
-for (let i = 0; i < iFrameElements.length; i++) {
-  updateIframeVideos(iFrameElements[i]);
+const sermonIframeElements = document.getElementsByClassName('sermon-iframe');
+for (let i = 0; i < sermonIframeElements.length; i++) {
+  updateIframeVideos(sermonIframeElements[i]);
 }
