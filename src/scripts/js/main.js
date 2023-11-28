60dad4413e715f15cb0c871968d5292bcee4d5e3
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-11-28 14:24:41 +0100

    refactor: Move iframe update function to module

--- src/scripts/js/main.js
+++ src/scripts/js/main.js
@@ -1,5 +1,7 @@
 // @ts-check
 
+import {updateIframeVideos} from './update-iframe-videos.js';
+
 const REV_CHANNEL_VIDEOS_URL = 'https://www.youtube.com/@NiyiEboda/videos';
 const SEEK_ID = '#video-title-link';
 const REV_CHANNEL_ID = 'UCWp3V0pGzOZy_an6ZsSUFiw';
@@ -10,34 +12,7 @@ const requestOptions = {
   redirect: 'follow',
 };
 
-/**
- * Function to update the videos displayed on the webpage
- * Modified from
- * @author Julia Furst Morgado
- * @address https://dev.to/juliafmorgado/automatically-embed-latest-youtube-videos-on-your-website-with-javascript-step-by-step-2i5e
- * @param {object} iframe - Node object to update the title and video source
- */
-const updateIframeVideos = (iframe) => {
-  const cid = REV_CHANNEL_ID;
-  const channelURL = encodeURIComponent(`https://www.youtube.com/feeds/videos.xml?channel_id=${cid}`);
-  const reqURL = `https://api.rss2json.com/v1/api.json?rss_url=${channelURL}`;
-
-  // @ts-ignore
-  fetch(reqURL, requestOptions)
-    .then(response => response.json())
-    .then(result => {
-      
-      const videoNumber = (iframe.getAttribute('video-number') ? Number(iframe.getAttribute('video-number')) : 0);
-      const link = result.items[videoNumber].link;
-      const videoTitle = result.items[videoNumber].title;
-      const id = link.substr(link.indexOf('=') + 1);
-      iframe.setAttribute('src', `https://youtube.com/embed/${id}?controls=0&autoplay=1`);
-      iframe.setAttribute('title', `${videoTitle}`);
-    })
-    .catch(err => console.error('Error:', err));
-};
-
 const sermonIframeElements = document.getElementsByClassName('sermon-iframe');
 for (let i = 0; i < sermonIframeElements.length; i++) {
-  updateIframeVideos(sermonIframeElements[i]);
+  updateIframeVideos(sermonIframeElements[i], REV_CHANNEL_ID, requestOptions);
 }
---
+++ src/scripts/js/update-iframe-videos.js
@@ -0,0 +1,32 @@
+// @ts-check
+
+/**
+ * Function to update the videos displayed on the webpage
+ * Modified from
+ * @author Julia Furst Morgado
+ * @address https://dev.to/juliafmorgado/automatically-embed-latest-youtube-videos-on-your-website-with-javascript-step-by-step-2i5e
+ * @param {object} iframe - Node object to update the title and video source
+ * @param {string} channelId - The unique id of the channel having the videos
+ * @param {object} requestOptions - Options to use with fetch()
+ */
+const updateIframeVideos = (iframe, channelId, requestOptions) => {
+  const cid = channelId;
+  const channelURL = encodeURIComponent(`https://www.youtube.com/feeds/videos.xml?channel_id=${cid}`);
+  const reqURL = `https://api.rss2json.com/v1/api.json?rss_url=${channelURL}`;
+
+  // @ts-ignore
+  fetch(reqURL, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      
+      const videoNumber = (iframe.getAttribute('video-number') ? Number(iframe.getAttribute('video-number')) : 0);
+      const link = result.items[videoNumber].link;
+      const videoTitle = result.items[videoNumber].title;
+      const id = link.substr(link.indexOf('=') + 1);
+      iframe.setAttribute('src', `https://youtube.com/embed/${id}?controls=0&autoplay=1`);
+      iframe.setAttribute('title', `${videoTitle}`);
+    })
+    .catch(err => console.error('Error:', err));
+};
+
+export { updateIframeVideos };
